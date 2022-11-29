adb7e558acf94be6da7d3594f90977693299315b
Darren Torchia <darren.torchia@aerian.com>
2022-11-29 15:11:34 +0000

    chore: tidy up v2

--- types/src/coord/axisAlignTicks.d.ts
+++
@@ -1,5 +0,0 @@
-import { NumericAxisBaseOptionCommon } from './axisCommonTypes';
-import IntervalScale from '../scale/Interval';
-import { AxisBaseModel } from './AxisBaseModel';
-import LogScale from '../scale/Log';
-export declare function alignScaleTicks(scale: IntervalScale | LogScale, axisModel: AxisBaseModel<Pick<NumericAxisBaseOptionCommon, 'min' | 'max'>>, alignToScale: IntervalScale | LogScale): void;
